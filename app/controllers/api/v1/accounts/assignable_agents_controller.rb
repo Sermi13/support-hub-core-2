f74d760a1f7ad41ab7829f6e8a91dd6496004ea7
Enrico <ricobordinx@gmail.com>
2025-08-22 14:03:08 -0300

    fix: Assignable agents show all agents now

--- app/controllers/api/v1/accounts/assignable_agents_controller.rb
+++ app/controllers/api/v1/accounts/assignable_agents_controller.rb
@@ -2,14 +2,17 @@ class Api::V1::Accounts::AssignableAgentsController < Api::V1::Accounts::BaseCon
   before_action :fetch_inboxes
 
   def index
-    agent_ids = @inboxes.map do |inbox|
-      authorize inbox, :show?
-      member_ids = inbox.members.pluck(:user_id)
-      member_ids
-    end
-    agent_ids = agent_ids.inject(:&)
-    agents = Current.account.users.where(id: agent_ids)
-    @assignable_agents = (agents + Current.account.administrators).uniq
+    ## Removed old original chatwoot logic
+    # agent_ids = @inboxes.map do |inbox|
+    #   authorize inbox, :show?
+    #   member_ids = inbox.members.pluck(:user_id)
+    #   member_ids
+    # end
+    # agent_ids = agent_ids.inject(:&)
+    # agents = Current.account.users.where(id: agent_ids)
+    # @assignable_agents = (agents + Current.account.administrators).uniq
+
+    @assignable_agents = Current.account.users.order_by_full_name.includes(:account_users, { avatar_attachment: [:blob] })
   end
 
   private
