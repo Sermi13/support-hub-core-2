760d10f8128eb0b2bb67194a5217f7cb165be1a8
Enrico <ricobordinx@gmail.com>
2025-08-22 09:58:01 -0300

    chore: Added all labels permissions for agent bots

--- app/controllers/concerns/access_token_auth_helper.rb
+++ app/controllers/concerns/access_token_auth_helper.rb
@@ -3,6 +3,7 @@ module AccessTokenAuthHelper
     'api/v1/accounts/conversations' => %w[toggle_status toggle_priority create update custom_attributes],
     'api/v1/accounts/conversations/messages' => ['create'],
     'api/v1/accounts/conversations/assignments' => ['create']
+    'api/v1/accounts/conversations/labels' => %w[create update destroy show index]
   }.freeze
 
   def ensure_access_token
